6173b9f57d1c6ca0ef0ca3fb53750e55d4f4253f
Spider-999 <harypatrascu.1@gmail.com>
2025-07-25 20:43:37 +0300

    update

--- workflow.sh
+++ workflow.sh
@@ -2,11 +2,21 @@
 
 GIT=`which git`
 
+if [ -z "$GIT" ]; then
+  echo "git is not installed, install git and try again."
+  exit 1
+fi
+
 cp -r org/roam/*.org Notes
+echo "------------------------------------------------------------"
 echo "copied new files to Notes"
+echo "------------------------------------------------------------"
 cd Notes
 # ${GIT} status
 ${GIT} pull origin main
+echo "------------------------------------------------------------"
 ${GIT} add *.org
 ${GIT} commit -m "new notes added"
+echo "------------------------------------------------------------"
 ${GIT} push origin main
+echo "------------------------------------------------------------"
